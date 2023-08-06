7b1d58e93ede4bafeaac844280a13bebd1af3c55
chadrackmbala <chadrackmbala20@gmail.com>
2023-08-06 13:43:30 +0100

    second commit : add of counter of buy

--- app-aliment.js
+++ app-aliment.js
@@ -1,2 +1,27 @@
-const ananasBtn = document.getElementById("ananas");
+const ananasBtn = document.getElementById("ananas-btn");
+const panier = document.getElementById("panier-container");
+const fruit = "Ananas"
+console.log(ananasBtn.textContent);
 
+let ananas = document.createElement("h4");
+    panier.appendChild(ananas);
+function ajouterFruit() {
+    ananas.textContent = fruit;
+}
+
+ananasBtn.addEventListener("click", ajouterFruit);
+
+let nombre = document.createElement("h4");
+panier.appendChild(nombre);
+console.log(nombre);
+
+let numberOfClick = 0;
+
+function compterClick() {
+    numberOfClick = numberOfClick + 1;
+    nombre.textContent = numberOfClick;
+    console.log(numberOfClick);
+}
+// ananas.textContent = numberOfClick, ananas;
+
+ananasBtn.addEventListener("click", compterClick);
